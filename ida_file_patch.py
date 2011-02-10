1db5d0a13fc5275a0a2d8a1d15aa99666930fa45
ero.carrera <ero.carrera@14ae8a5f-4bcc-fc46-4b87-1ef223690295>
2011-02-10 23:12:04 +0000

    Initial commit of working scripts ida_file_patch.py and ida_peid.py

---
+++ ida_file_patch.py
@@ -0,0 +1,105 @@
+# -*- coding: Latin-1 -*-
+"""IDA file patching script
+
+This simple script will scan the current IDB for changes and generate a
+patched file
+
+If the original input file can't be found at the original location where the
+IDB was generated from, it will prompt the user with a dialog to choose the
+original file. The original file is needed in order to compare the modifed
+data and prepare a patched file.
+
+The script will then scan the IDB for values that differ from the original
+ones. Data can be patched in the IDB through IDC/IDAPython function such as:
+
+  PatchByte()/PatchWord()/PatchDword()
+
+Once the changes have been collected the script will propmt for a new file
+where to store the patched data
+
+This script works with generic data files, PE files, ELF, raw data, etc
+As long as the modified data has a counterpart on the file, it will be patched
+Beware that if the user adds new segments those will have no counterpart in
+the original file and the script will ignore data in those.
+
+Copyright (c) 2011 Ero Carrera <ero.carrera@gmail.com>
+
+All rights reserved.
+"""
+
+__revision__ = "$LastChangedRevision: 00 $"
+__author__ = 'Ero Carrera'
+__version__ = '0.%d' % int( __revision__[21:-2] )
+__contact__ = 'ero.carrera@gmail.com'
+
+
+import os
+import idaapi
+
+
+def find_changed_bytes():
+
+    changed_bytes = list()
+
+    for seg_start in Segments():
+        for ea in range(seg_start, SegEnd(seg_start) ):
+            if isLoaded(ea):
+                byte = Byte(ea)
+                original_byte = GetOriginalByte(ea)
+                if byte != original_byte:
+                    changed_bytes.append( (ea, byte, original_byte) )
+            
+    return changed_bytes
+
+
+
+def patch_file(data, changed_bytes):
+    
+    for ea, byte, original_byte in changed_bytes:
+        print '%08x: %02x original(%02x)' % (ea, byte, original_byte)
+                
+        file_offset = idaapi.get_fileregion_offset( ea )
+        
+        original_char = chr( original_byte )
+        char = chr( byte )
+        
+        if data[ file_offset ] == original_char:
+            data[ file_offset ] = char
+    
+    patched_file = idc.AskFile( 1, '*.*', 'Choose new file')
+    if patched_file:
+        with file(patched_file, 'wb') as f:
+            f.write( ''.join( data ) )
+
+
+
+def main():
+    
+    print 'Finding changed bytes...',
+    changed_bytes = find_changed_bytes()
+    print 'done. %d changed bytes found' % len(changed_bytes)
+    
+    if changed_bytes:
+        original_file = GetInputFilePath()
+        print original_file
+    
+        if not os.path.exists(original_file):
+            original_file = idc.AskFile( 0, '*.*', 'Select original file to patch')
+        
+        if os.path.exists(original_file):
+
+            with file(original_file, 'rb') as f:
+                data = list( f.read() )
+
+            patch_file(data, changed_bytes)
+        
+        else:
+            print 'No valid file to patch provided'
+
+    else:
+        print 'No changes to patch'
+
+        
+print '---- Running script ----'
+main()
+print '---- Script finished ----'
---
+++ ida_peid.py
@@ -0,0 +1,81 @@
+# -*- coding: Latin-1 -*-
+"""IDA PEiD script
+
+This small script is intended as a small aid at finding possible packer code
+in an IDA database. It will run PEiD signatures at all locations defined as
+entry points.
+
+The script requires pefile: http://code.google.com/p/pefile/
+
+The PEiD signatures will be automatically fetched from google-code.
+The signatures used are those created by BoB / Team PEiD (with permission)
+
+Copyright (c) 2011 Ero Carrera <ero.carrera@gmail.com>
+
+All rights reserved.
+"""
+
+__revision__ = "$LastChangedRevision: 00 $"
+__author__ = 'Ero Carrera'
+__version__ = '0.%d' % int( __revision__[21:-2] )
+__contact__ = 'ero.carrera@gmail.com'
+
+import pefile
+import peutils
+
+peid_db_URL = 'http://reverse-engineering-scripts.googlecode.com/files/UserDB.TXT'
+
+
+def main():
+    inf = idaapi.get_inf_structure()
+
+    # Find the null character of the string (if any)
+    #
+    null_idx = inf.procName.find(chr(0))
+    if null_idx > 0:
+        processor_name = inf.procName[:null_idx]
+    else:
+        processor_name = inf.procName
+
+    if 'metapc' not in processor_name:
+        print 'This script can only handle x86 (metapc) and [%s] has been found' % processor_name
+        return
+        
+    print 'Found [%s] architecture, loading signatures...' % processor_name
+
+    try:
+        signatures = peutils.SignatureDatabase( peid_db_URL )
+    except Exception as excp:
+        print 'Unable to load PEiD signature databases'
+        print str(excp)
+        return
+
+    ep_count = GetEntryPointQty()
+    if ep_count == 0:
+        print 'The script could not find any entry points in the IDB'
+        print 'The signatures will be checked at the locations of entry points.'
+        print 'You can add entry points using the AddEntryPoint() function:'
+        print 'Documentation for AddEntryPoint():'
+        print '---------------------------------------------------------------'
+        print AddEntryPoint.__doc__
+        print '---------------------------------------------------------------'
+        return
+        
+    for ep_idx in range( ep_count ) :
+        ep_ord = GetEntryOrdinal( ep_idx )
+        ep = GetEntryPoint( ep_ord )
+        print 'Analyzing entry-point [%08x], %d of %d' % (ep, ep_idx+1, ep_count)
+        
+        data_length = 128
+        data = GetManyBytes(ep, data_length, 0)
+        if data is None or len(data) != data_length:
+            print 'Unable to read %d bytes at [%08x]' % (data_length, ep)
+            continue
+        
+        matches = signatures.match_data( data, ep_only = True )
+        if matches:
+            print 'Signatures matched at [%08x]: %s' % (ep, '; '.join( x[0] for x in matches[1] ))
+    
+print '---- Running script ----'
+main()
+print '---- Script finished ----'
